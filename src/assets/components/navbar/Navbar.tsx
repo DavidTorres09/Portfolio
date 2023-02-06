4fb6e1cd4072b2d08df19ed8b5b7d5fb3fe59b2c
DavidTorres09 <angeldtm09@gmail.com>
2023-02-06 17:58:44 -0500

    Changes in the navbar and the Css navbar. Little changes in the App.css

--- src/assets/components/navbar/Navbar.tsx
+++ src/assets/components/navbar/Navbar.tsx
@@ -1,41 +1,58 @@
 import React from "react";
 import './Navbar.css'
-import Facebook from '../../../icons/Facebook.png';
-import Linkedin from '../../../icons/Linkedin.png';
-import Twitter from '../../../icons/Twitter.png';
-import Whatsapp from '../../../icons/Whatsapp.png';
-import { NavLink } from "react-router-dom";
+
 
 const Navbar: React.FC = () => {
   return (
-    <nav>
-      <h1 id="name">Angel Torres Mora</h1>
-      <ul>
-        <li>
-          <a href="#" className="nav-link">HOME</a>
-        </li>
-        <li>
-          <a href="#" className="nav-link">PROJECTS</a>
-        </li>
-        <li>
-          <a href="#" className="nav-link">CONTACT</a>
-        </li>
-      </ul>
-      <div>
-        <a href="https://www.whatsapp.com/?lang=es" target="_blank">
-          <img src={Whatsapp} alt="" className="about-img" />
-        </a>
-        <a href="https://twitter.com/" target="_blank">
-          <img src={Twitter} alt="" className="about-img" />
-        </a>
-        <a href="https://www.facebook.com/" target="_blank">
-          <img src={Facebook} alt="" className="about-img" />
-        </a>
-        <a href="https://www.linkedin.com/" target="_blank">
-          <img src={Linkedin} alt="" className="about-img" />
-        </a>
-      </div>
-    </nav>
+    <header className="header">
+      <nav className="nav container">
+        <a href="index.html" className="nav__logo">Angel Torres</a>
+        <div className="nav__menu">
+          <ul className="nav__list grid">
+            <li className="nav__item">
+              <a href="#home" className="nav__link active-link">
+                <i className="uil uil-estate nav__icon">
+                </i>Home
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#about" className="nav__link">
+                <i className="uil uil-user nav__icon">
+                </i>About
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#skills" className="nav__link">
+                <i className="uil uil-briefcase-alt nav__icon">
+                </i>Skills
+              </a>
+            </li>
+            <li className="nav_item">
+              <a href="#services" className="nav__link">
+                <i className="uil uil-file-alt nav__icon">
+                </i>Services
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#portfolio" className="nav__link">
+                <i className="uil uil-scenery nav__icon">
+                </i>Portfolio
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#contact" className="nav__link">
+                <i className="uil uil-message nav__icon">
+                </i>Contact
+              </a>
+            </li>
+          </ul>
+          <i className="uil uil-times nav__close"></i>
+        </div>
+        <div className="nav__toggle">
+          <i className="uil uil-apps"></i>
+        </div>
+      </nav>
+    </header>
   );
 };
 
