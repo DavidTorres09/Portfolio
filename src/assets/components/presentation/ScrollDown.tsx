dfe3d1d7ee302cdf1b9c41840377ced145f9ced7
DavidTorres09 <angeldtm09@gmail.com>
2023-02-08 12:17:33 -0500

    Modified the CSS of Home and App. modified the ScrollDown tsx

--- src/assets/components/presentation/ScrollDown.tsx
+++ src/assets/components/presentation/ScrollDown.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Mouse from '../../../icons/Mouse.png';
+import Arrow from '../../../icons/ArrowDown.png';
 
-const ScrollDown = () =>{
-    return(
+const ScrollDown = () => {
+    return (
         <div className="home__scroll">
-            <a href="#about" className="home__scroll-button button--flex"></a>
+            <a href="#about" className="home__scroll-button button--flex">
+                <img src={Mouse} alt="Mouse" className="mouse-img" />
+                <span className="home__scroll-name">Scroll Down</span>
+                <img src={Arrow} alt="Mouse" className="arrow-img" />
+            </a>
         </div>
     )
 }
