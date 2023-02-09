9516338f08050fb9de665e3755fad257cd6b2759
DavidTorres09 <angeldtm09@gmail.com>
2023-02-09 12:37:21 -0500

    Insert the Html in skills, Backend and frontend

---
+++ src/assets/components/skills/Backend.tsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import Badge from '../../../icons/Badge.png';
+
+const Backend = () => {
+    return(
+        <div className="skills__content">
+            <h3 className="skills__title">Developer</h3>
+            <div className="skills__box">
+                <div className="skills__group">
+                    <div className="skills__data">
+                        <img src={Badge} alt="badge-img" className="badge__img" />
+                        <div>
+                            <h3 className="skills__name"></h3>
+                            <span className="skills__level"></span>
+                        </div>
+                    </div>
+                    <div className="skills__data">
+                        <img src={Badge} alt="badge-img" className="badge__img" />
+                        <div>
+                            <h3 className="skills__name"></h3>
+                            <span className="skills__level"></span>
+                        </div>
+                    </div>
+                    <div className="skills__data">
+                        <img src={Badge} alt="badge-img" className="badge__img" />
+                        <div>
+                            <h3 className="skills__name"></h3>
+                            <span className="skills__level"></span>
+                        </div>
+                    </div>
+                    <div className="skills__data">
+                        <img src={Badge} alt="badge-img" className="badge__img" />
+                        <div>
+                            <h3 className="skills__name"></h3>
+                            <span className="skills__level"></span>
+                        </div>
+                    </div>
+                </div>
+                <div className="skills__group">
+                    <div className="skills__data">
+                        <img src={Badge} alt="badge-img" className="badge__img" />
+                        <div>
+                            <h3 className="skills__name"></h3>
+                            <span className="skills__level"></span>
+                        </div>
+                    </div>
+                    <div className="skills__data">
+                        <img src={Badge} alt="badge-img" className="badge__img" />
+                        <div>
+                            <h3 className="skills__name"></h3>
+                            <span className="skills__level"></span>
+                        </div>
+                    </div>
+                    <div className="skills__data">
+                        <img src={Badge} alt="badge-img" className="badge__img" />
+                        <div>
+                            <h3 className="skills__name"></h3>
+                            <span className="skills__level"></span>
+                        </div>
+                    </div>
+                    <div className="skills__data">
+                        <img src={Badge} alt="badge-img" className="badge__img" />
+                        <div>
+                            <h3 className="skills__name"></h3>
+                            <span className="skills__level"></span>
+                        </div>
+                    </div>
+                    <div className="skills__data">
+                        <img src={Badge} alt="badge-img" className="badge__img" />
+                        <div>
+                            <h3 className="skills__name"></h3>
+                            <span className="skills__level"></span>
+                        </div>
+                    </div>
+                    <div className="skills__data">
+                        <img src={Badge} alt="badge-img" className="badge__img" />
+                        <div>
+                            <h3 className="skills__name"></h3>
+                            <span className="skills__level"></span>
+                        </div>
+                    </div>
+                
+                
+                
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Backend
---
+++ src/assets/components/skills/Frontend.tsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import Badge from '../../../icons/Badge.png';
+
+const Frontend = () => {
+    return (
+        <div className="skills__content">
+            <h3 className="skills__title">Developer</h3>
+            <div className="skills__box">
+                <div className="skills__group">
+                    <div className="skills__data">
+                        <img src={Badge} alt="badge-img" className="badge__img" />
+                        <div>
+                            <h3 className="skills__name">HTML</h3>
+                            <span className="skills__level">Medium</span>
+                        </div>
+                    </div>
+                    <div className="skills__data">
+                        <img src={Badge} alt="badge-img" className="badge__img" />
+                        <div>
+                            <h3 className="skills__name">CSS</h3>
+                            <span className="skills__level">Medium</span>
+                        </div>
+                    </div>
+                    <div className="skills__data">
+                        <img src={Badge} alt="badge-img" className="badge__img" />
+                        <div>
+                            <h3 className="skills__name">Javascript</h3>
+                            <span className="skills__level">Medium</span>
+                        </div>
+                    </div>
+                    <div className="skills__data">
+                        <img src={Badge} alt="badge-img" className="badge__img" />
+                        <div>
+                            <h3 className="skills__name">Typescript</h3>
+                            <span className="skills__level">Basic</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="skills__group">
+                    <div className="skills__data">
+                        <img src={Badge} alt="badge-img" className="badge__img" />
+                        <div>
+                            <h3 className="skills__name">React</h3>
+                            <span className="skills__level">Basic</span>
+                        </div>
+                    </div>
+                    <div className="skills__data">
+                        <img src={Badge} alt="badge-img" className="badge__img" />
+                        <div>
+                            <h3 className="skills__name">Java</h3>
+                            <span className="skills__level">Medium</span>
+                        </div>
+                    </div>
+                    <div className="skills__data">
+                        <img src={Badge} alt="badge-img" className="badge__img" />
+                        <div>
+                            <h3 className="skills__name">MySQL</h3>
+                            <span className="skills__level">Basic</span>
+                        </div>
+                    </div>
+                    <div className="skills__data">
+                        <img src={Badge} alt="badge-img" className="badge__img" />
+                        <div>
+                            <h3 className="skills__name">C#</h3>
+                            <span className="skills__level">Basic</span>
+                        </div>
+                    </div>
+                    <div className="skills__data">
+                        <img src={Badge} alt="badge-img" className="badge__img" />
+                        <div>
+                            <h3 className="skills__name">Git</h3>
+                            <span className="skills__level">Basic</span>
+                        </div>
+                    </div>
+                    <div className="skills__data">
+                        <img src={Badge} alt="badge-img" className="badge__img" />
+                        <div>
+                            <h3 className="skills__name">FireBase</h3>
+                            <span className="skills__level">Basic</span>
+                        </div>
+                    </div>
+                
+                
+                
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Frontend
